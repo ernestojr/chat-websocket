e7863d0cd8ba8dcc0e26aff9f2aaf6211595b855
ejrojasp <ejrojasp@falabella.cl>
2023-10-30 22:20:40 -0300

    Commit inicial.

---
+++ src/socket.js
@@ -0,0 +1,25 @@
+import { Server } from 'socket.io';
+
+let io;
+
+const conversation = [
+  {
+    username: 'coderhouse',
+    body: 'Hola a la comunidad de CH 🖐️.',
+  },
+];
+
+export const init = (httpServer) => {
+  io = new Server(httpServer);
+
+  io.on('connection', (socketClient) => {
+    
+    socketClient.emit('update-conversation', conversation);
+    
+    socketClient.on('new-message', (newMessage) => {
+      conversation.push(newMessage);
+      io.emit('update-conversation', conversation);
+    });
+
+  });
+};
